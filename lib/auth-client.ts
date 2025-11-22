e59748d807b733743b54c6f4c049f6d8fde72e12
apdyqafaar <qudhacle43@gmail.com>
2025-11-22 19:32:30 +0300

    solving deploment issue

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const {signIn, signUp, signOut, useSession, } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ai-chat-bot-lovat.vercel.app"
 })
 
 // export const { signIn, signUp, useSession } = createAuthClient()
