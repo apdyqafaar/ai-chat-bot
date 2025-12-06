7c698136b7bdd7d310bc22bed5d4b9ef34bfff57
apdyqafaar <qudhacle43@gmail.com>
2025-12-06 19:09:55 +0300

    Update Next.js to fix vulnerability

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const {signIn, signUp, signOut, useSession, } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://ai-chat-bot-lovat.vercel.app"
+    baseURL: "https://ai-chat-bot-qudhacles-projects.vercel.app"
 })
 
 // export const { signIn, signUp, useSession } = createAuthClient()
